2d526ad37fb5f21d590dac8380ae665b7b430e9d
Kmetha <rakursdead@gmail.com>
2022-09-01 14:23:22 +0300

    Initial commit

---
+++ src/main/java/ru/kmetha/patterns/GbPattrensExampleApplication.java
@@ -0,0 +1,13 @@
+package ru.kmetha.patterns;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GbPattrensExampleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GbPattrensExampleApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ru/kmetha/patterns/GbPattrensExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.kmetha.patterns;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GbPattrensExampleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
